3e41f1946770b893b01ae99cb75927de2c7ea9a6
Smitbhuva15 <itssmit123@gmail.com>
2025-06-22 12:56:31 +0000

    connect wallet

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hello34")
+import { createWalletClient, custom } from "https://esm.sh/viem";
+
+const connectButton = document.getElementById("connectButton");
+
+let walletClient;
+
+async function coonectFun() {
+    if (window.ethereum !== "undefined") {
+
+        walletClient = createWalletClient({
+            transport: custom(window.ethereum),
+        });
+
+        await walletClient.requestAddresses();
+        connectButton.innerHTML = "connected";
+
+
+    } else {
+        connectButton.innerHTML = "Please install MetaMask!";
+    }
+}
+
+connectButton.onclick = coonectFun;
